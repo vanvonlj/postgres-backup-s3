7eefbc4621dc24db808e6c27160444516c8f5143
Lucas Van Vonderen <56833898+vanvonlj@users.noreply.github.com>
2024-12-08 22:04:47 -0700

    Update backup.sh

--- src/backup.sh
+++ src/backup.sh
@@ -15,7 +15,7 @@ pg_dump --format=custom \
         > db.dump
 
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
-s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
+s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}.dump"
 
 if [ -n "$PASSPHRASE" ]; then
   echo "Encrypting backup..."
